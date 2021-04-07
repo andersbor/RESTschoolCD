41f5d4a391511739078c854bb6b2b24b961bdf9e
Anders Børjesson <anbo@zealand.dk>
2021-04-07 09:45:19 +0200

    Students controller GET ALL

---
+++ RESTschoolCD/Managers/StudentsManager.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using RESTschoolCD.Models;
+
+namespace RESTschoolCD.Managers
+{
+    public class StudentsManager
+    {
+        private static int _nextId = 1;
+        private static readonly List<Student> Data = new List<Student>()
+        {
+            new Student() {Id = _nextId++, Name = "Anders"}
+        };
+
+        public List<Student> GetAll()
+        {
+            return new List<Student>(Data);
+        }
+    }
+}
