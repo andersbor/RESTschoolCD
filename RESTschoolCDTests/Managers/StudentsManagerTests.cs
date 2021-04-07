dacadd769a81200a4d9b400123bd6950d3543350
Anders Børjesson <anbo@zealand.dk>
2021-04-07 09:52:10 +0200

    Test added

---
+++ RESTschoolCDTests/Managers/StudentsManagerTests.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using RESTschoolCD.Managers;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using RESTschoolCD.Models;
+
+namespace RESTschoolCD.Managers.Tests
+{
+    [TestClass()]
+    public class StudentsManagerTests
+    {
+        [TestMethod()]
+        public void GetAllTest()
+        {
+            StudentsManager manager = new StudentsManager();
+            List<Student> allStudents = manager.GetAll();
+            Assert.AreEqual(1, allStudents.Count);
+            Assert.AreEqual("Anders", allStudents[0].Name);
+        }
+    }
+}
