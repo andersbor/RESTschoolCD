d83e2c99278dd33fe848d9abbf6d24010f518b61
Anders Børjesson <anbo@zealand.dk>
2021-04-07 12:43:05 +0200

    GET all test

--- RESTschoolCDTests1/Managers/StudentsManagerTests.cs
+++ RESTschoolCDTests1/Managers/StudentsManagerTests.cs
@@ -1,18 +1,19 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
-using RESTschoolCD.Managers;
-using System;
 using System.Collections.Generic;
-using System.Text;
+using RESTschoolCD.Models;
 
 namespace RESTschoolCD.Managers.Tests
 {
     [TestClass()]
     public class StudentsManagerTests
     {
+        private readonly StudentsManager _manager = new StudentsManager();
+
         [TestMethod()]
         public void GetAllTest()
         {
-            Assert.Fail();
+            List<Student> allStudents = _manager.GetAll();
+            Assert.AreEqual(1, allStudents.Count);
         }
 
         [TestMethod()]
